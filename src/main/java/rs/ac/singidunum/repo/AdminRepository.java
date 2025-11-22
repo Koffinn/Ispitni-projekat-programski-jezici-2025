e743ffccd06dbe7b2f3ac10cf53bbe835a755fac
Mej Ivanović <maeivanovic8666@gmail.com>
2025-11-22 13:47:25 +0100

    Delete src directory

--- src/main/java/rs/ac/singidunum/repo/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.singidunum.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import rs.ac.singidunum.entity.Admin;
-import java.util.Optional;
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-    Optional<Admin> findByEmail(String email);
-}
